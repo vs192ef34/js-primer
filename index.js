9cce650fe97ab1a036cf41678c15e1bce4938e3f
Vlad <vskrashchuk@gmail.com>
2020-08-19 20:08:24 +0300

    Show ternary operator

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 /* eslint-disable no-console */
 
-// TODO: add more examples
+const a = 1;
+const b = 13;
+const c = 14;
+
+const check1 = a + b > c;
+const check2 = b + c > a;
+const check3 = c + a > b;
+
+const answer = check1 && check2 && check3;
+
+const action = answer ? "можно" : "нельзя";
+
+console.log(
+  `На сторонах с длинами (${a}, ${b}, ${c}) ${action} построить треугольник.`
+);
