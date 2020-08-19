7a24b778b36dc3d3f055d189671bcab4cff5ccd2
Vlad <vskrashchuk@gmail.com>
2020-08-19 20:59:43 +0300

    Show if-then

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const check1 = a + b > c;
 const check2 = b + c > a;
 const check3 = c + a > b;
 
-const answer = check1 && check2 && check3;
+const isTriangle = check1 && check2 && check3;
 
-const action = answer ? "можно" : "нельзя";
+let action = "можно";
+
+if (!isTriangle) {
+  action = "нельзя";
+}
 
 console.log(
   `На сторонах с длинами (${a}, ${b}, ${c}) ${action} построить треугольник.`
