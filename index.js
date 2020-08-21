5c26d1d90d039612f7978ec67410bc96be44e548
Vlad <vskrashchuk@gmail.com>
2020-08-21 20:51:21 +0300

    Show arrays and cycles

--- index.js
+++ index.js
@@ -1,35 +1,61 @@
 /* eslint-disable no-console */
 
+const a = [];
+
 for (let i = 1; i <= 10; i += 1) {
-  if (i % 2 !== 0) {
-    const sq = i * i;
-    console.log(`i = ${i}; sq = ${sq}`);
+  a.push(i);
+}
+
+for (let i = 0; i < a.length; i += 1) {
+  console.log(`a[${i}] = ${a[i]}`);
+}
+
+const b = [];
+
+for (let i = 0; i < 10; i += 1) {
+  b[i] = (i + 1) ** 2;
+}
+
+for (let i = 0; i < b.length; i += 1) {
+  console.log(`b[${i}] = ${b[i]}`);
+}
+
+// c = a + b => c[i] = a[i] + b[i]
+
+const c = [];
+
+for (let i = 0; i < b.length; i += 1) {
+  c.push(a[i] + b[i]);
+}
+
+for (let i = 0; i < c.length; i += 1) {
+  console.log(`c[${i}] = ${c[i]}`);
+}
+
+let sum = 0;
+let count = 0;
+
+for (let i = 0; i < c.length; i += 1) {
+  if (c[i] % 10 === 0) {
+    sum += c[i];
+    count += 1;
   }
 }
 
-let i = 90;
-while (i <= 10) {
-  console.log(`while ${i}`);
-  i += 1;
+let avarage = sum / count;
+console.log(sum, count, avarage);
+
+sum = 0;
+count = 0;
+
+for (let i = 0; i < c.length; i += 1) {
+  if (i % 2 !== 0) {
+    sum += c[i];
+    count += 1;
+
+    console.log(`c[${i}] = ${c[i]}, sum = ${sum}, count = ${count}`);
+  }
 }
 
-i = 90;
-do {
-  console.log(`do-while ${i}`);
-  i += 1;
-} while (i <= 10);
-
-const askUser = () => 23;
-
-console.log("Пожалуйста введите корректный возраст");
-let ages = askUser();
-while (ages < 18) {
-  console.log("Пожалуйста введите корректный возраст");
-  ages = askUser();
-}
-
-ages = 0;
-do {
-  console.log("Пожалуйста введите корректный возраст");
-  ages = askUser();
-} while (ages < 18);
+avarage = sum / count;
+console.log(sum, count, avarage);
