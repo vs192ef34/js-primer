47d0355cc01ff288172e8cbda382481b04044406
Vlad <vskrashchuk@gmail.com>
2020-09-02 21:17:04 +0300

    Show functions and closure

--- index.js
+++ index.js
@@ -26,29 +26,81 @@ console.log(
   `**********************************************************************`
 );
 
-const generateMatrix = () => {
+// const valueGeneratorMul = (i, j) => (i + 1) * (j + 1);
+// const valueGeneratorSum = (i, j) => i + j;
+// const valueGeneratorIdx = (i, j) => `${i}:${j}`;
+// const valueGeneratorRnd = (i, j) =>
+//   Math.floor(Math.random() * Math.floor(i * j));
+
+const generators = [
+  (i, j) => (i + 1) * (j + 1),
+  (i, j) => i + j,
+  (i, j) => `${i}:${j}`,
+  (i, j) => Math.floor(Math.random() * Math.floor(i * j)),
+];
+
+const generateMatrix = (rows, cols, valueGenerator) => {
   const matrix = [];
 
-  for (let i = 0; i < 9; i += 1) {
+  for (let i = 0; i < rows; i += 1) {
     const row = [];
-    for (let j = 0; j < 9; j += 1) {
-      row.push((i + 1) * (j + 1));
+    for (let j = 0; j < cols; j += 1) {
+      row.push(valueGenerator(i, j));
     }
     matrix.push(row);
   }
   return matrix;
 };
 
-const printMatrix = (matrix) => {
-  for (let i = 0; i < 9; i += 1) {
+const tabFormatter = (value) => `${value.toString()}\t`;
+
+const indicatorFormatter = (value) => {
+  if (value <= 50) {
+    return `-=${value.toString()}=-\t`;
+  }
+
+  return `+*${value.toString()}*+\t`;
+};
+
+const printMatrix = (matrix, formatter) => {
+  for (let i = 0; i < matrix.length; i += 1) {
     let row = "";
-    for (let j = 0; j < 9; j += 1) {
-      row += `${matrix[i][j].toString()}\t`;
+    const rowLength = matrix[i].length;
+
+    for (let j = 0; j < rowLength; j += 1) {
+      row += formatter(matrix[i][j]);
     }
+
     console.log(row);
   }
 };
 
-const matrix = generateMatrix();
+const generatedMatrixes = [];
 
-printMatrix(matrix);
+for (let i = 0; i < generators.length; i += 1) {
+  generatedMatrixes.push(generateMatrix(5, 5, generators[i]));
+}
+
+for (let i = 0; i < generatedMatrixes.length; i += 1) {
+  printMatrix(generatedMatrixes[i], tabFormatter);
+  console.log(
+    `====================================================================`
+  );
+}
+
+// const m = generateMatrix(16, 12, valueGeneratorRnd);
+// printMatrix(m, indicatorFormatter);
+
+// замыкание и пример возврата функции из другой функции
+function outer(multiplier) {
+  return (x) => x * multiplier;
+}
+
+const inner = outer(18);
+
+console.log(inner(3));
+console.log(inner(6));
+console.log(inner(12));
+console.log(inner(24));
+
+console.log(outer(2)(5));
